aec2ccda9f4070ed0045d0a174a71b2614035505
g-montanaro <giuliomontanaro98@gmail.com>
2023-05-11 14:20:23 +0300

    grebe 1

---
+++ pypy.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from phenospy import *
+import os
+
+# Get the current directory
+current_dir = os.getcwd()
+
+# -----------------------------------------
+# ARGUMENTS
+# -----------------------------------------
+phs_file    = os.path.join(current_dir, 'grebennikovius_descriptions.phs')
+yaml_file   = os.path.join(current_dir, 'phs-config.yaml')
+save_dir    = os.path.join(current_dir, 'output/')
+save_pref   = 'Grebennikovius'
+
+# -----------------------------------------
+# Convert PHS to OWL and XML
+# -----------------------------------------
+phsToOWL(phs_file, yaml_file, save_dir, save_pref)
+
+# -----------------------------------------
+# OWL to Markdown
+# -----------------------------------------
+# get owl file
+owl_file = os.path.join(save_dir, save_pref + '.owl')
+
+# Make NL graph
+onto = owlToNLgraph(owl_file)
+
+# Convert NL graph to Markdown
+taxon = 'Grebennikovius'
+file_md = os.path.join(current_dir, 'md', 'Grebennikovius.md')
+ind0 = onto.search(label = taxon)[0]
+NLgraphToMarkdown(onto, ind0, file_save = file_md, verbose =False)
